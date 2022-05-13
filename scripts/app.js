ef552c9c306a8e275b70318edeeb29116e22035b
Edgar <tugel@Edgaridz.com>
2022-05-13 10:43:29 +0300

    Через liveserver

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,28 @@
-var organizeByTags = function (toDoObjects) {
+var organizeByTags = function (toDoObjects) { 
 
 	console.log("organizeByTags called");
 	// создание пустого массива для тегов
 	var tags = [];
-	// перебираем все задачи toDos
+	// перебираем все задачи toDos 
 	toDoObjects.forEach(function (toDo) {
-		// перебираем все теги для каждой задачи
+		// перебираем все теги для каждой задачи 
 		toDo.tags.forEach(function (tag) {
 			// убеждаемся, что этого тега еще нет в массиве
-			if (tags.indexOf(tag) === -1) {
+			if (tags.indexOf(tag) === -1) { 
 				tags.push(tag);
 			}
 		});
-	});
+	}); 
 	console.log(tags);
 
 	var tagObjects = tags.map(function (tag) {
 		// здесь мы находим все задачи,
 		// содержащие этот тег
-		var toDosWithTag = [];
+		var toDosWithTag = []; 
 		toDoObjects.forEach(function (toDo) {
 			// проверка, что результат
 			// indexOf is *не* равен -1
-			if (toDo.tags.indexOf(tag) !== -1) {
+			if (toDo.tags.indexOf(tag) !== -1) { 
 				toDosWithTag.push(toDo.description);
 			}
 		});
@@ -42,7 +42,7 @@ var main = function (toDoObjects) {
 	});
 	// сейчас весь старый код должен работать в точности как раньше!
 	// ...
-
+    
     $("document").ready(function () {
 		$(".tabs a span").toArray().forEach(function (element) {
 			$(element).on("click", function () {
@@ -50,7 +50,7 @@ var main = function (toDoObjects) {
 				$(".tabs a span").removeClass("active");
 				$(element).addClass("active");
 				$("main .content").empty();
-
+    
                 if ($element.parent().is(":nth-child(1)")) {
 					for (var i = toDos.length - 1; i > -1; i--) {
 						$(".content").append($("<li>").text(toDos[i]));
@@ -62,22 +62,23 @@ var main = function (toDoObjects) {
 					});
 				}
 				else if ($element.parent().is(":nth-child(3)")) {
-				    var organizedByTag = organizeByTags(toDoObjects);
-
-					organizedByTag.forEach(function (tag) {
-						var $tagName = $("<h3>").text(tag.name),
-						$content = $("<ul>");
-						tag.toDos.forEach(function (description) {
-							var $li = $("<li>").text(description);
+					var organizedByTag = organizeByTags(toDoObjects);
+ 
+					organizedByTag.forEach(function (tag) { 
+						var $tagName = $("<h3>").text(tag.name), 
+						$content = $("<ul>"); 
+						tag.toDos.forEach(function (description) { 
+							var $li = $("<li>").text(description); 
 							$content.append($li);
 						});
-						$("main .content").append($tagName);
+						$("main .content").append($tagName); 
 						$("main .content").append($content);
 					});
+
 				}
 
-				else if ($element.parent().is(":nth-child(4)")) {
-					var $input = $("<input>").addClass("description"),
+				else if ($element.parent().is(":nth-child(4)")) { 
+					var $input = $("<input>").addClass("description"), 
 					$inputLabel = $("<h5>").text("Новая задача: "),
 					$tagInput = $("<input>").addClass("tags"),
 					$tagLabel = $("<h5>").text("Тэги: "),
@@ -85,8 +86,8 @@ var main = function (toDoObjects) {
 					$button.on("click", function () {
 						var description = $input.val(),
 						// разделение в соответствии с запятыми
-						tags = $tagInput.val().split(",");
-						toDoObjects.push({"description":description, "tags":tags});
+						tags = $tagInput.val().split(","); 
+						toDoObjects.push({"description":description, "tags":tags}); 
 						// обновление toDos
 						toDos = toDoObjects.map(function (toDo) {
 							return toDo.description;
@@ -94,7 +95,7 @@ var main = function (toDoObjects) {
 						$input.val("");
 						$tagInput.val("");
 					});
-				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button)
+				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button); 
 				}
 				return false;
 			})
